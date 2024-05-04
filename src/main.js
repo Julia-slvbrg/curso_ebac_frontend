953593248a8e44cdd69d507f0ba096c7d07ecdb7
Julia-slvbrg <julia.slvb@gmail.com>
2024-05-03 21:50:15 -0300

    exercício concluído

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var multi = function (a, b) {
+    return a * b;
+};
+console.log(multi(2, 4));
+var greet = function (name) {
+    return "Ol\u00E1 ".concat(name);
+};
+console.log('Júlia');
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+const multi = (a: number, b: number): number => {
+    return a*b
+};
+
+console.log(multi(2, 4));
+
+
+const greet = (name: string): string => {
+    return `Olá ${name}`
+}
+
+console.log('Júlia');
